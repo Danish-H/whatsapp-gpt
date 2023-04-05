b8c030411983358b5abad04eff8e8ea473474d74
Danish-H <me@danishhumair.com>
2023-04-04 18:03:08 -0600

    Basic stuff

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Copyright 2023 Danish Humair. All rights reserved.");
+
+const config = require("./config.json");
+
+console.log("Loaded config.json");
+console.log("Prefix: "+config.prefix);
