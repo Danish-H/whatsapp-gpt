fa52af7d6f38cb0a2c3bfc7344aca13f590d63c8
Danish-H <me@danishhumair.com>
2023-04-04 19:14:01 -0600

    Added WhatsApp integration and triggers

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 console.log("Copyright 2023 Danish Humair. All rights reserved.");
 
 const config = require("./config.json");
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 
-console.log("Loaded config.json");
-console.log("Prefix: "+config.prefix);
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+function randomDelay(min, max) {
+    return Math.round(min*1000 + (Math.random()*(max-min)*1000))
+}
+
+client.on('qr', qr => {
+    console.log("Please scan the following QR code to authenticate:")
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log("(!) Client is ready!");
+});
+
+client.initialize();
+
+client.on('message', async msg => {
+    console.log(`[${msg.timestamp}] [CID:${msg.from}] [T:${msg.type}] [M:${msg.hasMedia}] ${msg.author}: ${msg.body}`);
+
+    if (config.enabled_triggers.includes("fire") && msg.body.toLowerCase().includes("fire")) {
+        console.log("Reacting with fire emoji...");
+        await msg.react("🔥");
+    }
+
+    // if (msg.body.startsWith(config.prefix)) {
+    // }
+});
