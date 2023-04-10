68aa4d1fe5fb2875d0a71a0f4fa7028ff3b3387a
Danish Humair <me@danishhumair.com>
2023-04-10 01:56:27 +0000

    Cleaned up transcribe

--- commands/transcribe.js
+++ commands/transcribe.js
@@ -15,24 +15,26 @@ module.exports.run = async (bot, msg, args) => {
     try {
         if (msg.hasQuotedMsg) {
             const quote = await msg.getQuotedMessage();
-            console.log(quote.type);
+
             if (!quote.hasMedia || !(quote.type == "audio" || quote.type == "voice" || quote.type == "ptt")) {
                 await utils.naturalDelay();
                 await msg.reply("That message doesn't contain any audio! Please use *!help* for more information.");
-            } else {
-                const filename = Math.floor(100000000 + Math.random() * 900000000).toString();
-                const vn = await quote.downloadMedia();
-                let buffer = await Buffer.from(vn.data, 'base64');
-                await fs.writeFileSync(`./.cache/${filename}.ogg`, await buffer);
-                await child_process.execSync(`/usr/bin/ffmpeg -i .cache/${filename}.ogg -acodec libmp3lame .cache/${filename}.mp3`);
-                const transcript = await openai.createTranscription(await fs.createReadStream(`./.cache/${filename}.mp3`), 'whisper-1');
-                await utils.naturalDelay(bot);
-                await msg.reply(`${utils.price(quote.duration*0.006/60)}\n${transcript.data.text}`);
-            } 
+                return;
+            }
+
+            const filename = msg.id.id;
+            const vn = await quote.downloadMedia();
+            let buffer = Buffer.from(vn.data, 'base64');
+            
+            fs.writeFileSync(`./.cache/${filename}.ogg`, buffer);
+            child_process.execSync(`/usr/bin/ffmpeg -i .cache/${filename}.ogg -acodec libmp3lame .cache/${filename}.mp3`);
+            const transcript = await openai.createTranscription(fs.createReadStream(`./.cache/${filename}.mp3`), 'whisper-1');
+            
+            await utils.naturalDelay(bot);
+            await msg.reply(`${utils.price(quote.duration*0.006/60)}\n${transcript.data.text}`); 
         }
     } catch (error) {
-        console.log(error);
-        await console.log(error.response);
+        console.log(error.response);
         await utils.naturalDelay(bot);
         await msg.react('⚠️');
     }
