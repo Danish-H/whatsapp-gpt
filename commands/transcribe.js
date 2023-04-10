ffc279003e9311fe83b128c47e0a99bae33da16d
Danish Humair <me@danishhumair.com>
2023-04-10 02:05:15 +0000

    More cleaning

--- commands/transcribe.js
+++ commands/transcribe.js
@@ -13,26 +13,28 @@ const openai = new OpenAIApi(configuration);
 
 module.exports.run = async (bot, msg, args) => {
     try {
-        if (msg.hasQuotedMsg) {
-            const quote = await msg.getQuotedMessage();
-
-            if (!quote.hasMedia || !(quote.type == "audio" || quote.type == "voice" || quote.type == "ptt")) {
-                await utils.naturalDelay();
-                await msg.reply("That message doesn't contain any audio! Please use *!help* for more information.");
-                return;
-            }
-
-            const filename = msg.id.id;
-            const vn = await quote.downloadMedia();
-            let buffer = Buffer.from(vn.data, 'base64');
-            
-            fs.writeFileSync(`./.cache/${filename}.ogg`, buffer);
-            child_process.execSync(`/usr/bin/ffmpeg -i .cache/${filename}.ogg -acodec libmp3lame .cache/${filename}.mp3`);
-            const transcript = await openai.createTranscription(fs.createReadStream(`./.cache/${filename}.mp3`), 'whisper-1');
-            
+        if (!msg.hasQuotedMsg) {
             await utils.naturalDelay(bot);
-            await msg.reply(`${utils.price(quote.duration*0.006/60)}\n${transcript.data.text}`); 
+            await msg.react('❔');
+            return;
         }
+        
+        const quote = await msg.getQuotedMessage();
+        if (!quote.hasMedia || !(quote.type == "audio" || quote.type == "voice" || quote.type == "ptt")) {
+            await utils.naturalDelay();
+            await msg.reply("That message doesn't contain any audio! Please use *!help* for more information.");
+            return;
+        }
+
+        const filename = msg.id.id;
+        const vn = await quote.downloadMedia();
+        let buffer = Buffer.from(vn.data, 'base64');
+        fs.writeFileSync(`./.cache/${filename}.ogg`, buffer);
+        child_process.execSync(`/usr/bin/ffmpeg -i .cache/${filename}.ogg -acodec libmp3lame .cache/${filename}.mp3`);
+        const transcript = await openai.createTranscription(fs.createReadStream(`./.cache/${filename}.mp3`), 'whisper-1');
+        
+        await utils.naturalDelay(bot);
+        await msg.reply(`${utils.price(quote.duration*0.006/60)}\n${transcript.data.text}`);
     } catch (error) {
         console.log(error.response);
         await utils.naturalDelay(bot);
